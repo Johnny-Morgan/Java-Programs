dc18694b31c541e9735acd739a5f777e05267699
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-05-23 14:47:31 +0100

    Added menu and final static fields

--- Lotto.java
+++ Lotto.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 /**
  * Created by Johnny on 20/05/2019
- * COMMENTS ABOUT THE PROGRAM GO HERE
+ * Program to generate lottery numbers for Irish Lotto & Euromillions
  */
 public class Lotto {
 
+    private static Scanner kb = new Scanner(System.in);
+
+    private final static int NUM_OF_LOTTO_BALLS = 47;
+    private final static int NUM_OF_EUROMILLIONS_BALLS = 50;
+    private final static int NUM_OF_LUCKYSTAR_BALLS = 12;
+
     public static void shuffleLotto(int[] nums) {
         Random r = new Random();
-        for (int i = 0; i < 47; i++) {
-            int shuffle = r.nextInt(47);
+        for (int i = 0; i < NUM_OF_LOTTO_BALLS; i++) {
+            int shuffle = r.nextInt(NUM_OF_LOTTO_BALLS);
             int temp = nums[i];
             nums[i] = nums[shuffle];
             nums[shuffle] = temp;
@@ -20,8 +26,8 @@ public class Lotto {
 
     public static void shuffleEuro(int[] nums) {
         Random r = new Random();
-        for (int i = 0; i < 50; i++) {
-            int shuffle = r.nextInt(50);
+        for (int i = 0; i < NUM_OF_EUROMILLIONS_BALLS; i++) {
+            int shuffle = r.nextInt(NUM_OF_EUROMILLIONS_BALLS);
             int temp = nums[i];
             nums[i] = nums[shuffle];
             nums[shuffle] = temp;
@@ -30,8 +36,8 @@ public class Lotto {
 
     public static void shuffleEuroLuckyStar(int[] nums) {
         Random r = new Random();
-        for (int i = 0; i < 12; i++) {
-            int shuffle = r.nextInt(12);
+        for (int i = 0; i < NUM_OF_LUCKYSTAR_BALLS; i++) {
+            int shuffle = r.nextInt(NUM_OF_LUCKYSTAR_BALLS);
             int temp = nums[i];
             nums[i] = nums[shuffle];
             nums[shuffle] = temp;
@@ -39,11 +45,12 @@ public class Lotto {
     }
 
     public static void printNumbersEuro(int noOfLines, int[] mainNumbers, int[] luckyStarNumbers) {
-        System.out.println("Your Euromillions numbers are:");
+        System.out.println("\nYour Euromillions numbers are:");
+        System.out.println("\t\t\t\t\t\t\t\tLucky Star");
         for (int i = 1; i <= noOfLines; i++) {
             shuffleEuro(mainNumbers);
             shuffleEuroLuckyStar(luckyStarNumbers);
-            System.out.print("Line " + i + ": ");
+            System.out.print("Line " + i + ": \t");
 
             // create array to store first 5 numbers of shuffled array
             int[] myMainNums = new int[5];
@@ -62,20 +69,20 @@ public class Lotto {
             Arrays.sort(myLuckyStarNums);
 
             for (int num = 0; num < 5; num++) {
-                System.out.print(myMainNums[num] + " ");
+                System.out.print(myMainNums[num] + "\t");
             }
             for (int num = 0; num < 2; num++) {
-                System.out.print(myLuckyStarNums[num] + " ");
+                System.out.print(myLuckyStarNums[num] + "\t");
             }
             System.out.println();
         }
     }
 
     public static void printNumbersLotto(int noOfLines, int[] numbers) {
-        System.out.println("Your Lotto numbers are:");
+        System.out.println("\nYour Lotto numbers are:");
         for (int i = 1; i <= noOfLines; i++) {
             shuffleLotto(numbers);
-            System.out.print("Line " + i + ": ");
+            System.out.print("Line " + i + ": \t");
 
             // create array to store first 6 numbers of shuffled array
             int[] myNums = new int[6];
@@ -86,37 +93,51 @@ public class Lotto {
             Arrays.sort(myNums);
 
             for (int num = 0; num < 6; num++) {
-                System.out.print(myNums[num] + " ");
+                System.out.print(myNums[num] + "\t");
             }
             System.out.println();
         }
     }
 
+    public static int menu() {
+        System.out.println("Which lottery do you wish to play?");
+        System.out.println("1. Lotto");
+        System.out.println("2. Euromillions");
+        System.out.print("Choose 1 or 2: ");
+        return kb.nextInt();
+    }
+
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
 
         // initialise array with 47 numbers for Irish Lotto
-        int[] lottoNumbers = new int[47];
-        for (int i = 0; i < 47; i++) {
+        int[] lottoNumbers = new int[NUM_OF_LOTTO_BALLS];
+        for (int i = 0; i < NUM_OF_LOTTO_BALLS; i++) {
             lottoNumbers[i] = i + 1;
         }
+
         // initialise array with 50 numbers for Euromillions
-        int[] euroNumbers = new int[50];
-        for (int i = 0; i < 50; i++) {
+        int[] euroNumbers = new int[NUM_OF_EUROMILLIONS_BALLS];
+        for (int i = 0; i < NUM_OF_EUROMILLIONS_BALLS; i++) {
             euroNumbers[i] = i + 1;
         }
 
         // initialise array with 12 numbers for Euromillions Lucky Star
-        int[] luckyNumbers = new int[12];
-        for (int i = 0; i < 12; i++) {
+        int[] luckyNumbers = new int[NUM_OF_LUCKYSTAR_BALLS];
+        for (int i = 0; i < NUM_OF_LUCKYSTAR_BALLS; i++) {
             luckyNumbers[i] = i + 1;
         }
 
-        System.out.print("How many lines do you want to play: ");
+        int choice = menu();
+        System.out.print("\nHow many lines do you want to play: ");
         int lines = kb.nextInt();
 
-        //printNumbersLotto(lines, lottoNumbers);
-        printNumbersEuro(lines, euroNumbers, luckyNumbers);
+        switch (choice) {
+            case 1:
+                printNumbersLotto(lines, lottoNumbers);
+                break;
+            case 2:
+                printNumbersEuro(lines, euroNumbers, luckyNumbers);
+        }
         System.out.println("Good luck!");
     }
 }
