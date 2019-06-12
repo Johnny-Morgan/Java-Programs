74c82ce053d5fb57914e41d3c42d6b64fb573127
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-06-12 23:45:07 +0100

    Initial commit

---
+++ MexicanWave.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Johnny on 12/06/2019
+ * Program to create a method that turns a string into a Mexican Wave.
+ * The method is passed a string and must return that string in an array
+ * where an uppercase letter is a person standing up.
+ * If the character in the string is whitespace then pass over it as if it was an empty seat.
+ * Example: wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+ */
+public class MexicanWave {
+    public static String[] wave(String str) {
+        List<String> myList = new ArrayList<>();
+        char[] myChars;
+
+        for (int i = 0; i < str.length(); i++) {
+            myChars = str.toCharArray();
+            myChars[i] = Character.toUpperCase(myChars[i]);
+            if (myChars[i] == ' ')
+                continue;
+            myList.add(new String(myChars));
+        }
+        return myList.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        // test wave() method
+        System.out.println(Arrays.toString(wave("hello")));
+    }
+}
