41577198fa89a62102fcd3d41099e62478a2eaf2
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-06-13 00:04:11 +0100

    Second commit

--- MexicanWave.java
+++ MexicanWave.java
@@ -11,20 +11,17 @@ import java.util.List;
  * Example: wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
  */
 public class MexicanWave {
+
     public static String[] wave(String str) {
         List<String> myList = new ArrayList<>();
-        char[] myChars;
 
         for (int i = 0; i < str.length(); i++) {
-            myChars = str.toCharArray();
-            myChars[i] = Character.toUpperCase(myChars[i]);
-            if (myChars[i] == ' ')
+            if(str.charAt(i) == ' ')
                 continue;
-            myList.add(new String(myChars));
+            myList.add(str.substring(0 , i) + Character.toUpperCase(str.charAt(i)) + str.substring(i + 1));
         }
         return myList.toArray(new String[0]);
     }
-
     public static void main(String[] args) {
         // test wave() method
         System.out.println(Arrays.toString(wave("hello")));
