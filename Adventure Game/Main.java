c7be918404e4afb59e7398be91f57c139166c536
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-07-12 00:03:32 +0100

    Added exits

--- Adventure Game/Main.java
+++ Adventure Game/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Main {
 
     private static Map<Integer, Location> locations = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
@@ -18,14 +19,43 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the woods"));
 
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 5);
+        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+        locations.get(5).addExit("Q", 0);
+
         int loc = 1;
-        while(true) {
+        while (true) {
             System.out.println(locations.get(loc).getDescription());
-            if(loc == 0) {
+            if (loc == 0) {
                 break;
             }
-            loc = keyboard.nextInt();
-            if(!locations.containsKey(loc)) {
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.println("Available exits are ");
+            for (String exit : exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = keyboard.nextLine().toUpperCase();
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
                 System.out.println("You cannot go in that direction");
             }
         }
